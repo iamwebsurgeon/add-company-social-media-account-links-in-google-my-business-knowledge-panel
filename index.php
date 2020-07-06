f7483bae47ae27374b1301da5471dda718842eca
Mehmood Khalil <38806418+iamwebsurgeon@users.noreply.github.com>
2020-07-06 14:45:26 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<script type="application/ld+json"> { 
+  "@context" : "http://schema.org", 
+  "@type" : "Organization", 
+  "name" : "Your Organization Name", 
+  "url" : "http://www.your-site.com", 
+  "sameAs" : [ 
+    "http://www.facebook.com/your-profile", 
+    "http://www.instagram.com/your-profile", 
+    "http://www.twitter.com/yourProfile"
+  ] 
+} </script>
